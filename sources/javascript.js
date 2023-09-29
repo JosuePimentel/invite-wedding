0516b8178c103081f3c806ce032185c859d5db97
JosuePimentel <josue.farias.pimentel@gmail.com>
2023-09-29 13:27:27 -0300

    new changes

--- sources/javascript.js
+++ sources/javascript.js
@@ -9,3 +9,22 @@ bg.addEventListener("click", e => {
         behavior: "smooth"
     })
 })
+
+setTimeout(() => {
+    const sect_children = document.querySelector("section#Third").children
+    let i = 0
+    setInterval(() => {
+    // pointer.classList.toggle("animate")
+        if(i%2 == 0) {
+            sect_children.item(1).innerHTML = "left_click"
+        } else {
+            sect_children.item(1).innerHTML = "ads_click"
+        } 
+
+        if(i == 5) {
+            sect_children.item(0).style.opacity = 1
+            sect_children.item(2).style.opacity = 1
+        }
+        i++
+    }, 1000)
+}, 2000)
